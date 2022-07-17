f2be2fd54b8e3914a38ee11889edc51da0d2ef67
sunshiningsoo <yenkee1219@gmail.com>
2022-07-18 00:18:10 +0900

    Code base UIKit!

---
+++ UIKitDictionary/UIKitDictionary/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  UIKitDictionary
+//
+//  Created by 박성수 on 2022/07/17.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+        view.backgroundColor = .red
+    }
+
+
+}
+
