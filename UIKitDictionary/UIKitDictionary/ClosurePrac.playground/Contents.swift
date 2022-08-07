63b7e14b8a6019b1d16d064760aae8e51ab2b6fe
sunshiningsoo <yenkee1219@gmail.com>
2022-08-07 21:37:37 +0900

    closure 실제 calculator 사용예시

--- UIKitDictionary/UIKitDictionary/ClosurePrac.playground/Contents.swift
+++ UIKitDictionary/UIKitDictionary/ClosurePrac.playground/Contents.swift
@@ -18,18 +18,33 @@ func multiple(no1: Int, no2: Int) -> Int {
 calculate(a: 2, b: 3, operation: multiple)
 
 //MARK: - 클로저의 축약되는 과정
-{ (no1: Int, no2: Int) -> Int
+//{ (no1: Int, no2: Int) -> Int in
+//    return no1 * no2
+//}
+//
+//
+//{ (no1, no2) -> Int in
+//    return no1 * no2
+//}
+//
+//
+//{ (no1, no2) in no1 * no2 }
+//
+//{ ($0, $1) in $0 * $1 }
+//
+//{ $0 * $1 }
+
+//MARK: - 실제 축약하며 사용
+calculate(a: 2, b: 3, operation: { (no1: Int, no2: Int) -> Int in
     return no1 * no2
-}
+})
 
-
-{ (no1, no2) -> Int in
+calculate(a: 2, b: 3, operation: { (no1, no2) -> Int in
     return no1 * no2
-}
+})
 
+calculate(a: 2, b: 3, operation: { (no1, no2) in no1 * no2 })
 
-{ (no1, no2) in no1 * no2 }
+calculate(a: 2, b: 3, operation: { $0 * $1 })
 
-{ ($0, $1) in $0 * $1 }
-
-{ $0 * $1 }
+calculate(a: 2, b: 3) { $0 * $1 }
