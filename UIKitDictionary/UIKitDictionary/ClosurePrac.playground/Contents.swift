8be5a17e7223aa6ccc9afe229b9c18746fc62c1e
sunshiningsoo <yenkee1219@gmail.com>
2022-08-07 21:00:55 +0900

    Closure의 축약되는 과정

---
+++ UIKitDictionary/UIKitDictionary/ClosurePrac.playground/Contents.swift
@@ -0,0 +1,35 @@
+import Cocoa
+
+func calculate(a: Int, b: Int, operation: (Int, Int) -> Int) -> Int {
+    return operation(a, b)
+}
+
+func add(no1: Int, no2: Int) -> Int {
+    return no1 + no2
+}
+
+calculate(a: 2, b: 3, operation: add)
+
+
+func multiple(no1: Int, no2: Int) -> Int {
+    return no1 * no2
+}
+
+calculate(a: 2, b: 3, operation: multiple)
+
+//MARK: - 클로저의 축약되는 과정
+{ (no1: Int, no2: Int) -> Int
+    return no1 * no2
+}
+
+
+{ (no1, no2) -> Int in
+    return no1 * no2
+}
+
+
+{ (no1, no2) in no1 * no2 }
+
+{ ($0, $1) in $0 * $1 }
+
+{ $0 * $1 }
