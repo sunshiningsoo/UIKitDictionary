a6ea16dc6d88b4bf46b7d204c7b1c8620eb88df6
sunshiningsoo <yenkee1219@gmail.com>
2022-08-20 23:05:00 +0900

    뷰컨트롤러 라이프사이클 주석설명 추가

--- UIKitDictionary/UIKitDictionary/ViewLifeCycle/ViewLifeCycleViewController.swift
+++ UIKitDictionary/UIKitDictionary/ViewLifeCycle/ViewLifeCycleViewController.swift
@@ -26,11 +26,15 @@ class ViewLifeCycleViewController: UIViewController {
     }
     
     override func loadView() {
-        super.loadView()
+        // 뷰를 만들어주는 역할, 만들어 메모리에 올라가면 viewDidLoad가 호출됨
+        super.loadView() // super.loadView()가 없다면 무한루프에 갖힘 앱이 켜지지 않음
         print("loadView1")
     }
     
     override func viewDidLoad() {
+        // 처음 1번만 실행 되어야 하는 코드들
+        // 리소스 초기화, 초기 화면을 구성하는 용도
+        // 다른 뷰로 갔다가 돌아오는 상황에서는 로드되지 않음 navigation의 rootView 라면 재호출 되지 않는다는 말
         super.viewDidLoad()
         print("ViewDidLoad1")
         
@@ -39,12 +43,14 @@ class ViewLifeCycleViewController: UIViewController {
     }
 
     override func viewWillAppear(_ animated: Bool) {
+        // 다른 뷰로 이동후 돌아와 설정해주어야 하는 값들을 설정해주어야 한다. viewDidLoad가 재호출 되지 않음
         super.viewWillAppear(animated)
         print("viewWillAppear1")
         view.backgroundColor = .systemBackground
     }
     
     override func viewDidAppear(_ animated: Bool) {
+        // 화면에 그려줄 애니메이션을 처리해주기도 함
         super.viewDidAppear(animated)
         print("viewDidAppear1")
     }
